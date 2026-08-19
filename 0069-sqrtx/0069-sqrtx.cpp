a0a845f8422810e213c4ffc2ee380e75f8332115
Vishnu Singh <vishnu8429singhk@gmail.com>
2026-08-19 09:24:26 +0530

    Time: 1 ms (22.04%), Space: 8.3 MB (99.62%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int n) {
+      long long int start=0;
+     long long int end = n;
+      
+      int ans=-1;
+      while(start<=end){
+        long long int mid=start+(end-start)/2;
+       long long int square = mid*mid;
+        if(square==n)
+        return mid;
+        if(square<n){
+            ans=mid;
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+        mid=start+(end-start)/2;
+      }   
+      return ans;
+    }
+};
